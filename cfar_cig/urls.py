3c7aa655ef0e70781ddb826f33e6df0b5e919c79
Scott White <whitews@gmail.com>
2013-04-12 13:57:11 -0400

    first draft of website

---
+++ cfar_cig/urls.py
@@ -0,0 +1,27 @@
+from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', TemplateView.as_view(template_name="home.html"), name='home'),
+    url(r'^register$', TemplateView.as_view(template_name="register.html"), name='register'),
+    url(r'^agenda/1$', TemplateView.as_view(template_name="day_1.html"), name='day_1'),
+    url(r'^agenda/2$', TemplateView.as_view(template_name="day_2.html"), name='day_2'),
+    url(r'^agenda/3$', TemplateView.as_view(template_name="day_3.html"), name='day_3'),
+    url(r'^agenda/4$', TemplateView.as_view(template_name="day_4.html"), name='day_4'),
+    url(r'^agenda/5$', TemplateView.as_view(template_name="day_5.html"), name='day_5'),
+    url(r'^hotels$', TemplateView.as_view(template_name="hotels.html"), name='hotels'),
+    url(r'^contact', TemplateView.as_view(template_name="contact.html"), name='contact'),
+
+    # url(r'^cfar_cig/', include('cfar_cig.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
---
+++ cfar_cig/wsgi.py
@@ -0,0 +1,38 @@
+"""
+WSGI config for cfar_cig project.
+
+This module contains the WSGI application used by Django's development server
+and any production WSGI deployments. It should expose a module-level variable
+named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
+this application via the ``WSGI_APPLICATION`` setting.
+
+Usually you will have the standard Django WSGI application here, but it also
+might make sense to replace the whole Django WSGI application with a custom one
+that later delegates to the Django one. For example, you could introduce WSGI
+middleware here, or combine a Django application with an application of another
+framework.
+
+"""
+import os
+import sys
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cfar_cig.settings")
+
+paths = [
+    '/srv/django-projects/cfar_cig',
+    '/srv/django-projects/cfar_cig/cfar_cig'
+]
+
+for path in paths:
+    if path not in sys.path:
+        sys.path.append(path)
+
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+# Apply WSGI middleware here.
+# from helloworld.wsgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
